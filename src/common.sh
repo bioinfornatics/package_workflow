7bba6ce79c36e5737777f5458c823fd0dd63d671
bioinfornatics <bioinfornatics@fedoraproject.org>
2013-08-07 09:08:01 +0200

    Stable and works as expected

--- src/common.sh
+++ src/common.sh
@@ -1,8 +1,9 @@
 
 
 declare -x needToBump=false
-declare -x suourceUpdated=false
+declare -x sourceUpdated=false
 declare -x isInitialized=false
+declare -x needSendSource=false
 declare -x snapdate
 declare -x revision
 declare -x release
@@ -77,21 +78,25 @@ init () {
     isInitialized=true
     [[ -e /proc/$$/fd/3 ]] && die ${LINENO} 'File descriptor 3 already used' 1
     [[ -e /proc/$$/fd/4 ]] && die ${LINENO} 'File descriptor 4 already used' 1
-    [[ $verbose == true ]] && echo 'Starting to process package '"${BOLD}${package_name}${RESET}"' on branch '"${BOLD}${branch}${RESET}"' ( '"${logDir}"' )'
+    
+    ${verbose} && echo 'Starting to process package '"${BOLD}${package_name}${RESET}"' on branch '"${BOLD}${branch}${RESET}"' ( '"${logDir}"' )'
+    
     exec 3>&1  1>>"${logOut}" # Merqe fd 1 with fd 3 and Redirect to logOut file
     exec 4>&2  2>>"${logErr}" # Merqe fd 2 with fd 4 and Redirect to logErr file
 
-    if [[ ! -f "${specFile}" ]]; then
-        ( cd "${SPECS}" && fedpkg clone "${package_name}" )
-    fi
-
+    [[ ! -f "${specFile}" ]] && ( cd "${SPECS}" && fedpkg clone "${package_name}" )
 
     pushd "${SPECS}"/"${package_name}"/ 1> /dev/null
         fedpkg switch-branch ${branch} 1> /dev/null
         fedpkg pull 1> /dev/null
     popd  1> /dev/null
-
-    getSourcesFiles
+        
+	getSourcesFiles
+	sendNewSources
+	if ${needSendSource}; then
+		fedpkg new-sources ${sourcesFiles[@]}
+        fedpkg commit -m 'Upload new sources' -p
+    fi
 }
 
 
@@ -112,6 +117,27 @@ end (){
 }
 
 
+# Check if a value exists in an array
+# Parameter:
+# - $1 item to search into given array  
+# - $2 array  
+# return  Success (0) if value exists, Failure (1) otherwise
+inArray () {
+    local item="$1"
+    local array=( $2 )
+    local exists=1
+    local -i index=0
+    while [[ $index -lt ${#array[@]} ]]; do
+		if [[ ${array[$index]} == $item ]]; then
+			index=${#array[@]}
+			exists=0
+		fi
+		((index++))
+    done
+    return $exists
+}
+
+
 # bumpSpec
 # This function is used to increase the release number
 # You do not have need to call it as this function is called from localBuild and udpateSpec.
@@ -119,8 +145,9 @@ end (){
 # - comment to write into the spec file
 bumpSpec () {
     local comment
-    [[ $# -eq 1 ]]                  || die ${LINENO} "bumpSpec expected a comment" 1
-    [[ $isInitialized == true ]]    || die ${LINENO} "Error: you need to run init fuction at beginning" 1
+    [[ $# -eq 1 ]] || die ${LINENO} "bumpSpec expected a comment" 1
+    ${isInitialized} || die ${LINENO} "Error: you need to run init fuction at beginning" 1
+    
     comment="$1"
     rpmdev-bumpspec -u "$userstring" --comment="${comment}" "${tmpSpecFile}"
 }
@@ -131,19 +158,21 @@ bumpSpec () {
 # The build is done if package get an update or if variable force is true
 localBuild () {
     local fedora_version tmp srpms
-    [[ $# -eq 0 ]]                  || die ${LINENO} 'buildRPM expected 0 or 1 parameters not '"$#" 1
-    [[ $isInitialized == true ]]    || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
-    [[ $verbose == true ]]          && echo 'Building '"${package_name}"' rpms'
+    [[ $# -eq 0 ]] || die ${LINENO} 'buildRPM expected 0 or 1 parameters not '"$#" 1
+    ${isInitialized} || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    
+    ${verbose} && echo 'Building '"${package_name}"' rpms'
+    
 	if [[ "${branch}" == "master" ]]; then
 		fedora_version='fedora-rawhide-'"$(uname -m)"
 	else
 		fedora_version="${branch/f/fedora-}"'-'"$(uname -m)"
 	fi
-    if [[ $needToBump == false  && $force == true ]]; then
+    if ${needToBump} && ${force}; then
         bumpSpec "Rebuild" || die ${LINENO} 'bumpspec failure!' 1
     fi
 
-    if [[ $needToBump == true  || $force == true ]]; then
+    if ${needToBump} || ${force} ; then
         echo '==== Building '"${package_name}"' rpms ===='
         tmp=$( rpmbuild -bs "${tmpSpecFile}" )
         if [[ "${tmp}" =~ ${SRPMS}(.+\.rpm) ]]; then
@@ -164,19 +193,77 @@ localBuild () {
 }
 
 
+# remoteBuild
+# This function is used to do a remote build by using fedpkg tool
+# The build is done if package get an update or if variable force is true
+# Parameter:
+# - comment to use when commiting
+remoteBuild () {
+    local comment untracked item
+    [[ "$#" -eq 0 || "$#" -eq 1 ]]  || die ${LINENO} "updatePackage expected 0 or 1 parameters not $#" 1
+    [[ -n "$1" ]] && comment="$1" || comment='Rebuild'
+    
+    if ${needToBump} ; then
+        pushd "${SPECS}"/"${package_name}"/  1> /dev/null
+        
+        ${verbose} && echo 'Updating package '${package_name}'from branch '${branch}
+		
+        cp "${tmpSpecFile}" "${specFile}"
+        untracked=( $(git ls-files -o) )
+        
+        for item in "${untracked[@]}"; do
+            if [[ $(basename "${specFile}") == "${item}" ]]; then
+                git add "${item}"
+            fi
+        done
+        
+		getSourcesFiles
+        
+        fedpkg new-sources ${sourcesFiles[@]}
+        
+        fedpkg commit -m "${comment}" -p
+        fedpkg build
+        #bodhi -u $login -c "${comment}" -N "${comment}" --type='enhancement' ${package_name}
+        popd  1> /dev/null
+    else
+        echo "${package_name}"' already up to date. Nothing to do.'
+    fi
+    
+}
+
+
+# build
+# Thit function run localBuild and remoteBuild function
+# Parameter:
+# - comment to use when commiting
+build () {
+	local comment=''
+	
+    [[ "$#" -eq 0 || "$#" -eq 1 ]]  || die ${LINENO} "updatePackage expected 0 or 1 parameters not $#" 1
+    [[ -n "$1" ]] && comment="$1"
+    
+    localBuild
+    remoteBuild "${comment}"
+	
+}
+
+
 # getLinkToRPMS
 # This function return an url to rpm package and set package_link variable
 # This allow to download it and by example use rpmdiff
 getLinkToRPMS () {
     local line architecture
-    [[ $isInitialized == true ]]    || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
-    [[ $# -eq 0 ||  $# -eq 1 ]]     || die ${LINENO} 'Error: getLinkToRPMS take 0 prameter or an architecture name as x86_64…' 1
-    [[ $# -eq 1 ]] && architecture="$1"  || architecture=''
+    ${isInitialized} 					|| die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    [[ $# -eq 0 ||  $# -eq 1 ]]    		|| die ${LINENO} 'Error: getLinkToRPMS take 0 prameter or an architecture name as x86_64…' 1
+    [[ $# -eq 1 ]] && architecture="$1" || architecture=''
+    
     package_link=()
+    
     while read line; do
         [[ "${line}" =~ ^ftp|http.+${package_name}.*\."${architecture}"\.rpm ]] && package_link+=("${line}")
         [[ "${line}" =~ ^ftp|http.+${package_name}.*\.noarch\.rpm ]] && package_link+=("${line}")
     done < <(yumdownloader --destdir=${logDir} --urls ${package_name})
+    
     [[ ${#package_link[@]} -ne 0 ]] ||  die ${LINENO} 'Error: getLinkToRPMS fail to found url to download the rpm' 1
 }
 
@@ -186,14 +273,17 @@ getLinkToRPMS () {
 # downloadRPMS <arch>
 downloadRPMS () {
     local architecture
-    [[ $isInitialized == true ]]            || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    ${isInitialized}            			|| die ${LINENO} 'Error: you need to run init fuction at beginning' 1
     [[ $# -eq 1 ]]  && architecture="$1"    || die ${LINENO} 'Error: downloadRPMS take an architecture name as x86_64…' 1
+    
     getLinkToRPMS "${architecture}"
+    
     if [[ "${package_link[0]}" =~ .+(${package_name}.+\.rpm) ]]; then # assume is first index 0
         oldRPM="${logDir}/${BASH_REMATCH[1]}"
     else
         die ${LINENO} 'RPMS name not found into: '"${package_link[0]}" 1
     fi
+    
     curl -s -o "${oldRPM}" "${package_link[0]}"
 }
 
@@ -203,12 +293,14 @@ downloadRPMS () {
 # This function allow to get list of generated rpm file by reading log file
 getRPMS () {
     local line buildLogFile
-    [[ $isInitialized == true ]]        || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    ${isInitialized}        			|| die ${LINENO} 'Error: you need to run init fuction at beginning' 1
     [[ $# -eq 1 ]] && buildLogFile="$1" || die ${LINENO} 'Error: getRPMS expct the build log file as parameter' 1
     [[ -f "${buildLogFile}" ]]          || die ${LINENO} 'Error: The build log file do not exist: '"${buildLogFile}" 1
+    
     for line in $(grep -o -e "${package_name}.*\.rpm" "${buildLogFile}"); do
         rpmsList+=("${logDir}/${line}")
     done
+    
     [[ ${#rpmsList[@]} -ne 0 ]] || die ${LINENO} 'Error: generated file rpm are not found into log file: '"${buildLogFile}" 1
 }
 
@@ -219,7 +311,7 @@ getRPMS () {
 getSpecRelease () {
     local isSearching eof
     local -r pattern='^Release:[[:blank:]]+([[:digit:]]+)'
-    [[ $isInitialized == true ]]    || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    ${isInitialized}        			|| die ${LINENO} 'Error: you need to run init fuction at beginning' 1
     isSearching=true
 
     while isSearching; do
@@ -246,11 +338,11 @@ getSpecRelease () {
 udpateSpec () {
     local comment parameters index match line pattern value tmpfile
     [[ $# -ge 1 ]]                                  || die ${LINENO} 'udpadeSpec expected at least 1 parameters not '$# 1
-    [[ $isInitialized == true ]]                    || die ${LINENO} 'Error: you need to run init fuction at beginning' 1
+    ${isInitialized}                    			|| die ${LINENO} 'Error: you need to run init fuction at beginning' 1
     [[ -n "$1" ]]                && comment="$1"    || die ${LINENO} 'udpadeSpec expected a comment as second parameter' 1
     shift
     
-    [[ $verbose == true ]] && echo "Updating spec file: ${specFile}"
+    ${verbose} && echo "Updating spec file: ${specFile}"
 
     if [[ -n "$@" ]]; then
         [[ $(( $# % 2 )) ]]  && parameters=( "$@" ) || die ${LINENO} 'udpadeSpec expected a paired numbers of parameters' 1
@@ -269,16 +361,16 @@ udpateSpec () {
             done
             echo "${line}" >> "${tmpfile}"
         done < "${specFile}"
-        if ${needToBump} ; then
-            mv "${tmpfile}" "${tmpSpecFile}"
-        fi
+        
+        ${needToBump} && mv "${tmpfile}" "${tmpSpecFile}"
     else
         needToBump=true
     fi
     
-    if ${needToBump} ; then
-        bumpSpec "${comment}"
-    fi
+    if ${needToBump}; then
+		bumpSpec "${comment}"
+	fi
+    
 }
 
 
@@ -288,6 +380,7 @@ udpateSpec () {
 getSourcesFiles () {
     local line sourceFile url key hashValue varValue varname
     local -A variables
+    
     while read line; do
         if [[ "${line}" =~ ^%global[[:blank:]]+([[:alnum:]_]+)[[:blank:]]+([[:alnum:][:punct:]]+) ]]; then
             varname='%{'"${BASH_REMATCH[1]}"'}'
@@ -301,55 +394,28 @@ getSourcesFiles () {
                 done
             fi
             variables["${varname}"]="${varValue}"
-        elif [[ "${line}" =~ ^name:[[:blank:]]+([[:alnum:][:punct:]]+) ]]; then
+        elif [[ "${line}" =~ ^Name:[[:blank:]]+([[:alnum:][:punct:]]+) ]]; then
             variables['%{name}']="${BASH_REMATCH[1]}"
         elif [[ "${line}" =~ ^Version:[[:blank:]]+([[:digit:]\.]+) ]]; then
             variables['%{version}']="${BASH_REMATCH[1]}"
         elif [[ "${line}" =~ ^Patch|Source[[:digit:]]+:[[:blank:]]+([[:alnum:][:punct:]]+) ]]; then
             url="${BASH_REMATCH[1]}"
+            
             if [[ "${url}" =~ ^http|ftp ]]; then
                 sourceFile="${url##*/}"
             else
                 sourceFile="${url}"
             fi
+            
             for key in "${!variables[@]}"; do
                 sourceFile=${sourceFile//"${key}"/${variables["${key}"]}}
             done
+            
             sourcesFiles+=( "${SOURCES}"/"${sourceFile}" )  
         fi
     done < "${tmpSpecFile}"
+    
     [[ ${#sourcesFiles[@]} -ne 0 ]] || die ${LINENO} 'sources files List is epmty' 1
-}
-
-
-# remoteBuild
-# This function is used to do a remote build by using fedpkg tool
-# The build is done if package get an update or if variable force is true
-# Parameter:
-# - comment to use when commiting
-remoteBuild () {
-    local comment untracked item
-    [[ "$#" -eq 0 || "$#" -eq 1 ]]  || die ${LINENO} "updatePackage expected 0 or 1 parameters not $#" 1
-    [[ -n "$1" ]] && comment="$1" || comment='Rebuild'
-
-    if ${needToBump} ; then
-        pushd "${SPECS}"/"${package_name}"/  1> /dev/null
-        [[ $verbose == true ]] && echo 'Updating package '${package_name}'from branch '${branch}
-        cp "${tmpSpecFile}" "${specFile}"
-        untracked=( $(git ls-files -o) )
-        for item in "${untracked[@]}"; do
-            if [[ $(basename "${specFile}") == "${item}" ]]; then
-                git add "${item}"
-            fi
-        done
-        fedpkg new-sources ${sourcesFiles[@]}
-        fedpkg commit -m "${comment}" -p
-        fedpkg build
-        #bodhi -u $login -c "${comment}" -N "${comment}" --type='enhancement' ${package_name}
-        popd  1> /dev/null
-    else
-        echo "${package_name}"' already up to date. Nothing to do.'
-    fi
 }
 
 
@@ -361,7 +427,7 @@ gitGetRepo () {
     local repo
     [[ "$#" -eq 1 ]]                || die ${LINENO} "gitGetRepo expected 1 parameters not $#" 1
     [[ -n "$1" ]]   && repo="$1"    || die ${LINENO} 'gitGetRepo expected a git url repository' 1
-    [[ $isInitialized == true ]]    || die ${LINENO} "Error: you need to run init fuction at beginning" 1
+    ${isInitialized}        		|| die ${LINENO} "Error: you need to run init fuction at beginning" 1
     if [[ ! -d "${SOURCES}"/"${package_name}" ]]; then
         git clone "${repo}" "${SOURCES}"/"${package_name}" 1> /dev/null
         cd "${SOURCES}"/"${package_name}"
@@ -371,6 +437,7 @@ gitGetRepo () {
     fi
 }
 
+
 # gitExtractSnapDate
 # This function is used to get date from the latest commit.
 # The date is stored into the global variable 'snapdate'.
@@ -378,6 +445,7 @@ gitExtractSnapDate () {
     local date_string
     [[ $# -eq 0 ]]  || die ${LINENO} "gitExtractSnapDate expected 0 parameters not $#" 1
     [[ -e '.git' ]] || die ${LINENO} "Error: is not a git repository" 1
+    
     date_string=$(git log -1 --format="%ci")
     date_string="${date_string%% *}"
     snapdate="${date_string//-/}"
@@ -390,7 +458,8 @@ gitExtractSnapDate () {
 gitExtractRev () {
     [[ $# -eq 0 ]]          || die ${LINENO} "gitExtractRev expected 0 parameters not $#" 1
     [[ -e '.git' ]]         || die ${LINENO} "Error: is not a git repository" 1
-    [[ $verbose == true ]]  && echo "Extracting $(basename  $(pwd)) archive"
+    
+    ${verbose} && echo "Extracting $(basename  $(pwd)) archive"
     revision="$(git rev-parse --short HEAD)"
 }
 
@@ -405,9 +474,25 @@ gitArchive () {
     local package alphatag outputDir archive
     [[ $# -eq 3 ]]  || die ${LINENO} "gitArchive expected 3 parameters not $#" 1
     [[ -e '.git' ]] || die ${LINENO} "Error: is not a git repository" 1
+    
     prefix="$1"
     package="$2"
     outputDir="$3"
     archive=$( readlink -m "${outputDir}"/"${package}".tar.xz )
     [[ -f "${archive}" ]] || $(git archive --prefix="${prefix}"/ HEAD --format=tar | xz > "${archive}" )
+}
+
+
+# sendNewSources
+# This furction parse sources file and compare to source dercribe into .spec file via sourcesFiles array
+# if not equal needSendSource take false otherwise true
+sendNewSources () {
+	local needSendSource=false
+	
+	while read md5 file; do
+		if ! inArray "${SOURCES}/${file}" ${sourcesFiles[@]} ; then
+		    needSendSource=true
+		    break
+		fi
+	done < "${SPECS}/${package_name}/sources"
 }
